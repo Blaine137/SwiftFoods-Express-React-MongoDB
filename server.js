32756ed5fb1e1e41a5db41e7d2fbf7159f96b37b
Blaine Young <blaine.young3553@gmail.com>
2020-10-21 00:51:23 -0400

    added body parser and attempted to fetch data from db

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const Products = require('./models/products');
 require('dotenv/config');
 
 
@@ -9,7 +11,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 // app.use(express.static("client/build"));
-
+app.use(bodyParser.json());
 
 mongoose.connect(process.env.DB_CONNECTION,{ useNewUrlParser: true }, () => {
     console.log('connected to db')
@@ -24,7 +26,8 @@ shopRouter.route('/')
     next();
 })
 .get((req, res) => {
-    console.log('communicating')
+    res.send(req.body)
+    console.log(req.body)
 });
 
 app.use('/shop', shopRouter)
