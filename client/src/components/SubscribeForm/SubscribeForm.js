717232caa0673de7737960fdd0347c23d6a6bfbe
blaine young <blaine.young3553@gmail.com>
2020-11-13 15:31:12 -0500

    testing logged in users

--- client/src/components/SubscribeForm/SubscribeForm.js
+++ client/src/components/SubscribeForm/SubscribeForm.js
@@ -50,7 +50,7 @@ class SubscribeForm extends Component {
             //modal goes here
             return(
                 <div className="container">
-                    <h3>Thank you for subscribing!</h3>
+                    <h3 className="text-center mt-5 mb-5 align-self-center">Thank you for subscribing!</h3>
                 </div>
             );
         }else{
