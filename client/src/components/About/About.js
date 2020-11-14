54d66b6308982906926b439d9a93237db69e8aa7
blaine young <blaine.young3553@gmail.com>
2020-11-14 15:52:31 -0500

    fixed animations on about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -18,52 +18,52 @@ const About = (props) => {
                     </div>
                     <div class="container">
                         <div class="row row-content align-items-center">
-                            <Slide top>
-                                <div class="col-6 ">
+                            <div class="col-6 ">
+                                <Slide top>
                                     <h4 class="font-weight-bold">
                                         {props.data ? props.data.about.title1 : <Spinner color="#574AE2"/> }
                                     </h4>
-                                </div>
-                            </Slide>
-                            <Slide bottom>
-                                <div class="col text-left ">
+                                </Slide>
+                            </div>
+                            <div class="col text-left ">
+                                <Slide bottom>
                                     <p class="w-55 ml-auto">
                                         {props.data ? props.data.about.p1 : <Spinner color="#574AE2"/> }
                                     </p>
-                                </div>
-                            </Slide>
+                                </Slide>
+                            </div>
                         </div>
                         <div class="row row-content align-items-center">
-                            <Slide top>
-                                <div class="col text-left">
+                            <div class="col text-left">
+                                <Slide top>
                                     <p class="w-55 mr-auto">
                                         {props.data ? props.data.about.p2 : <Spinner color="#574AE2"/> }
                                     </p>
-                                </div>
-                            </Slide>
-                            <Slide bottom>
-                                <div class="col-6 text-right">
+                                </Slide>
+                            </div>
+                            <div class="col-6 text-right">
+                                <Slide bottom>
                                     <h4 class="font-weight-bold">
                                         {props.data ? props.data.about.title2 : <Spinner color="#574AE2"/> }
                                     </h4>
-                                </div>
-                            </Slide>
+                                </Slide>
+                            </div>
                         </div>
                         <div class="row row-content align-items-center">
-                            <Slide top>
-                                <div class="col-6">
+                            <div class="col-6">
+                                <Slide top>
                                     <h4 class="font-weight-bold">
                                         {props.data ? props.data.about.title3 : <Spinner color="#574AE2"/> }
                                     </h4>
-                                </div>
-                            </Slide>
-                            <Slide bottom>
-                                <div class="col text-left">
+                                </Slide>
+                            </div>
+                            <div class="col text-left">
+                                <Slide bottom>
                                     <p class=" w-55 ml-auto ">
                                         {props.data ? props.data.about.p3 : <Spinner color="#574AE2"/> }
                                     </p>
-                                </div>
-                            </Slide>
+                                </Slide>
+                            </div>
                         </div>
                     </div>
                     
