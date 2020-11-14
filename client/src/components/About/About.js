e853ec206937cc2869a105b07672459d351f988c
blaine young <blaine.young3553@gmail.com>
2020-11-14 16:14:20 -0500

    fixed animations on about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -11,7 +11,7 @@ const About = (props) => {
                         <div class="row">
                             <Fade top>
                                 <div class="col-12">
-                                    <h3 class="text-center mt-5 mb-5">About Us!</h3>
+                                    <h3 class="text-center mt-5 mb-5 ml-auto mr-auto">About Us!</h3>
                                 </div>
                             </Fade>
                         </div>
