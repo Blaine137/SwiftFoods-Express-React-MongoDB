c0310fc1ec6dc8456eda8890238d0cb945baed26
blaine young <blaine.young3553@gmail.com>
2021-03-10 01:21:34 -0500

    added items to cart

--- client/src/components/Cart/Cart.js
+++ client/src/components/Cart/Cart.js
@@ -9,7 +9,7 @@ import './Cart.scss';
 const Cart = ({formValues, addToCart, removeFromCart}) => {
 
     return(
-        <div className="h-75">
+        <>
             <div className="container w-75 pl-0 pr-0">
                 <div className="row mt-5">
                         <div className="col-12 align-items-left">
@@ -18,100 +18,102 @@ const Cart = ({formValues, addToCart, removeFromCart}) => {
                             </p>
                         </div>
                 </div>
-            </div>
-            <div className="container w-75 pl-0 pr-0 mt-2 mb-2">
+            
+                <div className="row align-items-center mb-4">
+                    <div className="col-3 d-flex justify-content-center">
+                        <FontAwesomeIcon 
+                            icon={faMinus}  
+                            size="2x"
+                            onClick={() => removeFromCart("pizza", 1)}
+                        /> 
+                    </div>
+                    <div className="col-3 d-flex justify-content-center">
+                        <Media 
+                            object 
+                            src={process.env.PUBLIC_URL + '/assets/images/peppizza.jpg'} 
+                            alt="pizza img" 
+                            className="d-inline rounded-circle cartImg" 
+                        />
+                    </div>
+                    <div className="col-3 d-flex justify-content-start">
+                        {formValues.pizzas} 
+                    </div>
+                    <div className="col-3 d-flex justify-content-start">
+                        <FontAwesomeIcon 
+                            icon={faPlus}  
+                            size="2x"
+                            onClick={() => addToCart("pizza", 1)}
+                        />
+                    </div>
+                </div>
+
+                <div className="row align-items-center mb-4">
+                    <div className="col-3 d-flex justify-content-center">
+                        <FontAwesomeIcon 
+                            icon={faMinus}  
+                            size="2x"
+                            onClick={() => removeFromCart("salad", 1)}
+                        /> 
+                    </div>
+                    <div className="col-3 d-flex justify-content-center">
+                        <Media 
+                            object 
+                            src={process.env.PUBLIC_URL + '/assets/images/salad.jpg'} 
+                            alt="pizza img" 
+                            className="d-inline rounded-circle cartImg" 
+                        />
+                    </div>
+                    <div className="col-3 d-flex justify-content-start">
+                        {formValues.pizzas} 
+                    </div>
+                    <div className="col-3 d-flex justify-content-start">
+                        <FontAwesomeIcon 
+                            icon={faPlus}  
+                            size="2x"
+                            onClick={() => addToCart("salad", 1)}
+                        />
+                    </div>
+                </div>
+
+                <div className="row align-items-center mb-4">
+                    <div className="col-3 d-flex justify-content-center">
+                        <FontAwesomeIcon 
+                            icon={faMinus}  
+                            size="2x"
+                            onClick={() => removeFromCart("burger", 1)}
+                        /> 
+                    </div>
+                    <div className="col-3 d-flex justify-content-center">
+                        <Media 
+                            object 
+                            src={process.env.PUBLIC_URL + '/assets/images/burger.jpg'} 
+                            alt="pizza img" 
+                            className="d-inline rounded-circle cartImg" 
+                        />
+                    </div>
+                    <div className="col-3 d-flex justify-content-start">
+                        {formValues.pizzas} 
+                    </div>
+                    <div className="col-3 d-flex justify-content-start">
+                        <FontAwesomeIcon 
+                            icon={faPlus}  
+                            size="2x"
+                            onClick={() => addToCart("burger", 1)}
+                        />
+                    </div>
+                </div>
+           
                 <div className="row align-items-center">
-                    <div className="col-12">
-                        <p className=" my-auto">
-                            <FontAwesomeIcon 
-                                icon={faMinus} 
-                                className="mr-5" 
-                                size="2x"
-                                onClick={() => removeFromCart("pizza", 1)}
-                            /> 
-                                <Media 
-                                    object 
-                                    src={process.env.PUBLIC_URL + '/assets/images/peppizza.jpg'} 
-                                    alt="pizza img" 
-                                    className="d-inline rounded-circle mr-3 cartImg" 
-                                />
-                                {formValues.pizzas} 
-                            <FontAwesomeIcon 
-                                icon={faPlus} 
-                                className="ml-5" 
-                                size="2x"
-                                onClick={() => addToCart("pizza", 1)}
-                            />
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="container w-75 pl-0 pr-0 mt-2 mb-2">
-                <div className="row align-self-center">
-                    <div className="col-12">
-                        <p className="my-auto">
-                            <FontAwesomeIcon 
-                                icon={faMinus} 
-                                className="mr-5" 
-                                size="2x"
-                                onClick={() => removeFromCart("salad", 1)}
-                            /> 
-                                <Media 
-                                    object 
-                                    src={process.env.PUBLIC_URL + '/assets/images/peppizza.jpg'} 
-                                    alt="pizza img" 
-                                    className="d-inline rounded-circle mr-3 cartImg" 
-                                />
-                                {formValues.salads} 
-                            <FontAwesomeIcon 
-                                icon={faPlus} 
-                                className="ml-5" 
-                                size="2x"
-                                onClick={() => addToCart("salad", 1)}
-                            />
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="container w-75 pl-0 pr-0 mt-2 mb-2">
-                <div className="row align-self-center">
-                    <div className="col-12">
-                        <p className="my-auto">
-                            <FontAwesomeIcon 
-                                icon={faMinus} 
-                                className="mr-5" 
-                                size="2x"
-                                onClick={() => removeFromCart("burger", 1)}
-                            /> 
-                                <Media 
-                                    object 
-                                    src={process.env.PUBLIC_URL + '/assets/images/peppizza.jpg'} 
-                                    alt="pizza img" 
-                                    className="d-inline rounded-circle mr-3 cartImg" 
-                                />
-                                {formValues.burgers} 
-                            <FontAwesomeIcon 
-                                icon={faPlus} 
-                                className="ml-5" 
-                                size="2x"
-                                onClick={() => addToCart("burger", 1)}
-                            />
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div className="container w-75 pl-0 pr-0 mt-2 mb-4">
-                <div className="row align-self-start">
-                    <div className="col-12">
-                        <Link to="/checkout" className="mr-auto">
-                            <Button className="orderBtn d-block mr-auto">
+                    <div className="col-12 d-flex justify-content-center">
+                        <Link to="/checkout" className="w-25">
+                            <Button className="checkoutBtn">
                                 Checkout
                             </Button>
                         </Link>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 
 };
